ad225cfa27c086cc187db0714dec7130dad399be
mclegacycodes <kingsleytwum2@gmail.com>
2023-03-09 10:18:27 +0000

    made the expense item component dynamic by using props

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ function App() {
   return (
     <div className="App">
       <h1>My name is mclegacy</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
+      <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
+      <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date}></ExpenseItem>
+      <ExpenseItem title={expense[3].title} amount={expense[3].amount} date={expense[3].date}></ExpenseItem>
     </div>
   );
 }
