a673d59cf0538780754e2bbf0d9bee1721eab3f2
mclegacycodes <kingsleytwum2@gmail.com>
2023-03-10 18:53:53 +0000

    two ways of handling multiple states

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -1,20 +1,40 @@
+import React,{useState} from 'react';
 import './NewExpenseForm.css';
 const NewExpenseForm=()=>{
+    // const [title,setTitle]=useState("");
+    // const [amount,setAmount]=useState(0);
+    // const [date,setDate]=useState("");
+    const[userInput,setUserInput]=useState({title:'', amount:'', date:''})
+
+    const titleHandler=(event)=>{
+        // setTitle(event.target.value);
+        setUserInput({...userInput, title:event.target.value});
+    }
+    const amountHandler=(event)=>{
+        // setAmount(event.target.value);
+        setUserInput({...userInput, amount:event.target.value});
+
+    }
+    const dateHandler=(event)=>{
+        // setDate(event.target.value);
+        setUserInput({...userInput, date:event.target.value});
+        console.log(userInput);
+    }
 return <form action="#">
     <div className='new-expense__controls'>
         <div className='new-expense__control'>
             <label htmlFor="title">Title</label>
-            <input type="text" name='title'/>
+            <input type="text" name='title' onChange={titleHandler}/>
 
         </div>
         <div className='new-expense__control'>
             <label htmlFor="amount">Amount</label>
-            <input type="number" name='amount' min="0.01" step="0.01"/>
+            <input type="number" name='amount' min="0.01" step="0.01" onChange={amountHandler}/>
 
         </div>
         <div className='new-expense__control'>
             <label htmlFor="date">Title</label>
-            <input type="date" min="2023-01-01" max="2023-12-31"/>
+            <input type="date" min="2023-01-01" max="2023-12-31" onChange={dateHandler}/>
 
         </div>
     </div>
